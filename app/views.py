6eddf35bfcdff9ceae8703e3811cbda08a9688e7
Juan <jlizarraga@unomaha.edu>
2022-03-03 20:10:58 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render, redirect
 
 # On the home page we can show a lists' webpage, for now we won't worry about user authentication
 def home(request):
+    
     return render (request, '../templates/home/home.html')
