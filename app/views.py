48c58c20acbe85d878ae2a67b75fe0b6ead8e051
Juan <jlizarraga@unomaha.edu>
2022-03-06 20:49:33 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from .models import list, item
 
 
 # Create your views here.
 
 # On the home page we can show a lists' webpage, for now we won't worry about user authentication
 def home(request):
+    my_lists = list.objects.all()
 
-    return render (request, '../templates/home/home.html')
+    return render (request, '../templates/home/home.html', {'my_lists' : my_lists})
