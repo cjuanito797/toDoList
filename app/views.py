7374f476678402dfaa8b8332f90eeead1379b9bc
Juan <jlizarraga@unomaha.edu>
2022-03-10 16:08:13 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import list, item
+from .forms import ListForm, ItemForm
 
 
 # Create your views here.
@@ -9,3 +10,18 @@ def home(request):
     my_lists = list.objects.all()
 
     return render (request, '../templates/home/home.html', {'my_lists' : my_lists})
+
+def new_list(request):
+    if request.method == "POST":
+        list_form = ListForm(request.POST)
+
+        if list_form.is_valid():
+            list = list_form.save()
+            list.save()
+
+            return redirect(reverse('app:home'))
+
+    else:
+        list_form = ListForm()
+
+    return render(request, "../templates/userActions/addList.html", {'list_form' : list_form, })
